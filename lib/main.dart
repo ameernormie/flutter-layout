fbe81c569ac2f342fd80372c3aac92291430e504
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-07-21 12:52:27 +0500

    chore: add text section in layout

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,19 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget textSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese'
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
         title: 'Flutter Layout Demo',
         theme: ThemeData(
@@ -67,7 +80,7 @@ class MyApp extends StatelessWidget {
             title: Text('Flutter Layout Demo'),
           ),
           body: ListView(
-            children: <Widget>[titleSection, buttonSection],
+            children: <Widget>[titleSection, buttonSection, textSection],
           ),
         ));
   }

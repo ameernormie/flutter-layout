1a85134f0928e731bc6620813dcd5e7cacc0f94a
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-07-22 01:20:31 +0500

    feat: add first statfull widget in the flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(Icons.star, color: Colors.red[500]),
-          Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -106,6 +105,51 @@ class MyApp extends StatelessWidget {
           child: Text(label,
               style: TextStyle(
                   fontSize: 12, fontWeight: FontWeight.w400, color: color)),
+        )
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavouriteWidgetState createState() => _FavouriteWidgetState();
+}
+
+class _FavouriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
         )
       ],
     );

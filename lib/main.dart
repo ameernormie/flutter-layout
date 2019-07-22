0729a6ca3a5ca6a43f2295fc27974aa323cb35b2
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-07-22 21:01:07 +0500

    chore: widget manages it's own state
    Practicing managing state in flutter, In this commit I learned how the
    widget manages it's own state

--- lib/main.dart
+++ lib/main.dart
@@ -88,7 +88,8 @@ class MyApp extends StatelessWidget {
               ),
               titleSection,
               buttonSection,
-              textSection
+              textSection,
+              TapboxA(),
             ],
           ),
         ));
@@ -152,6 +153,41 @@ class _FavouriteWidgetState extends State<FavoriteWidget> {
           ),
         )
       ],
+    );
+  }
+}
+
+// ****************** TapBoxA ******************** //
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(_active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
     );
   }
 }

3f10cf68c46ede1fee66e2e91958d3c15c7f0c59
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-07-23 00:12:45 +0500

    chore: parent and child widget both maintain state
        Practicing managing state in flutter, In this commit I learned how the state is managed by both parent and child

--- lib/main.dart
+++ lib/main.dart
@@ -91,6 +91,7 @@ class MyApp extends StatelessWidget {
               textSection,
               TapboxA(),
               ParentWidget(),
+              MixAndMatchParentWidget(),
             ],
           ),
         ));
@@ -245,6 +246,99 @@ class TapboxB extends StatelessWidget {
         height: 200.0,
         decoration: BoxDecoration(
           color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+// ******************  Mix and Match Parent Widget ******************** //
+class MixAndMatchParentWidget extends StatefulWidget {
+  @override
+  _MixAndMatchParentWidgetState createState() =>
+      _MixAndMatchParentWidgetState();
+}
+
+class _MixAndMatchParentWidgetState extends State<MixAndMatchParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+// ******************  TapboxC ******************** //
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  _TapboxCState createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  Widget build(BuildContext context) {
+    // This example adds a green border on tap down.
+    // On tap up, the square changes to the opposite state.
+    return GestureDetector(
+      onTapDown: _handleTapDown, // Handle the tap events in the order that
+      onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(widget.active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+                  color: Colors.teal[700],
+                  width: 10.0,
+                )
+              : null,
         ),
       ),
     );
